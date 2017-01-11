a7adc6f14d775e75817c2a14a0cb1ffdbb55dfc2
Marc Tönsing <marc.toensing@gmail.com>
2017-01-11 13:31:03 +0100

    Filter for Google Analytics Widget

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+function my_theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
-function wpse15850_body_class( $wp_classes, $extra_classes )
+/* Add has-sidebar class to body tag on single pages */
+function wpse15850_body_class($wp_classes, $extra_classes)
 {
-    if(is_single()) {
-
+    if (is_single()) {
         $blacklist = array('has-sidebar');
 
         $wp_classes = array_diff($wp_classes, $blacklist);
@@ -22,69 +22,66 @@ function wpse15850_body_class( $wp_classes, $extra_classes )
 }
 
 
+add_filter('body_class', 'wpse15850_body_class', 100, 2);
 
-    add_filter( 'body_class', 'wpse15850_body_class', 100, 2 );
-
-
-function custom_comments( $comment, $depth, $args ) {
-    $tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
-    ?>
-    <<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $comment->has_children ? 'parent' : '', $comment ); ?>>
-    <article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-        <footer class="comment-meta">
-            <div class="comment-author vcard">
-                <?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-                <?php
-                /* translators: %s: comment author link */
-                printf( __( '%s <span class="says">says:</span>' ),
-                    sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
-                );
-                ?>
-            </div><!-- .comment-author -->
-
-            <div class="comment-metadata">
-                <a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-                    <time datetime="<?php comment_time( 'c' ); ?>">
-                        <?php
-                        /* translators: 1: comment date, 2: comment time */
-                        printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
-                        ?>
-                    </time>
-                </a>
-                <?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
-            </div><!-- .comment-metadata -->
-
-            <?php if ( '0' == $comment->comment_approved ) : ?>
-                <p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
-            <?php endif; ?>
-        </footer><!-- .comment-meta -->
-
-        <div class="comment-content">
-            <?php comment_text(); ?>
-        </div><!-- .comment-content -->
-
-        <?php
-        comment_reply_link( array_merge( $args, array(
-            'add_below' => 'div-comment',
-            'depth'     => $depth,
-            'max_depth' => $args['max_depth'],
-            'before'    => '<div class="reply">',
-            'after'     => '</div>'
-        ) ) );
-        ?>
-    </article><!-- .comment-body -->
-    <?php
-}
-
-
-function anorlondo_content_width() {
-
+/* set content width to 740 (image width) */
+function anorlondo_content_width()
+{
     $content_width = 740;
 
-    if ( twentyseventeen_is_frontpage() ) {
+    if (twentyseventeen_is_frontpage()) {
         $content_width = 1120;
     }
 
-    $GLOBALS['content_width'] = apply_filters( 'twentyseventeen_content_width', $content_width );
+    $GLOBALS['content_width'] = apply_filters('twentyseventeen_content_width', $content_width);
 }
-add_action( 'after_setup_theme', 'anorlondo_content_width', 1 );
+
+add_action('after_setup_theme', 'anorlondo_content_width', 1);
+
+
+/* filter AMP and comment pages from google analytics widget */
+
+
+function super_unique($array, $key)
+{
+    $temp_array = array();
+    foreach ($array as &$v) {
+        if (!isset($temp_array[$v[$key]])) {
+            $temp_array[$v[$key]] =& $v;
+        }
+    }
+    $array = array_values($temp_array);
+    return $array;
+}
+
+
+function gtc_pages_filter_automatically_remove_items($pages)
+{
+    $strings_to_exclude = array(
+    '/amp/',
+    '/comment-page-',
+    '/blog/'
+  );
+
+if(count($pages) <= 0){
+  return $pages;
+}
+
+    foreach ($pages as $index => &$page) {
+
+      // get position of each string in array
+      foreach ($strings_to_exclude as $string) {
+          $last_string_start = strrpos($page['path'], $string);
+
+          if ($last_string_start !== false) {
+              $overflow = substr($page['path'], $last_string_start + 1, 1000);
+              $page['path'] = str_replace($overflow, "", $page['path']);
+          }
+      }
+    }
+    $pages = super_unique($pages, 'path');
+
+    return $pages;
+}
+
+add_filter('gtc_pages_filter', 'gtc_pages_filter_automatically_remove_items');
