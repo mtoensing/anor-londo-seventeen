6f639b6dde0ad9a1a9261a348978ce989df181d7
Marc Tönsing <marc.toensing@gmail.com>
2017-05-25 12:35:17 +0200

    new yarpp template

--- yarpp-template-thumbnails.php
+++ yarpp-template-thumbnails.php
@@ -9,23 +9,25 @@ Author: Marc Tönsing
     <div id="jp-relatedposts" class="jp-relatedposts" style="display: block;">
         <h3 class="jp-relatedposts-headline"><em>Ähnliche Beiträge</em></h3>
         <div class="jp-relatedposts-items jp-relatedposts-items-visual jp-relatedposts-grid ">
-        <?php $i = 0; ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <?php if (has_post_thumbnail()):?>
+			<?php $i = 0; ?>
+			<?php while (have_posts()) : the_post(); ?>
+				<?php if (has_post_thumbnail()):?>
 
-                <div class="jp-relatedposts-post jp-relatedposts-post<?php echo $i?> jp-relatedposts-post-thumbs">
-                    <a class="jp-relatedposts-post-a" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" rel="nofollow" >
-                        <img class="unveil" data-src="<?php the_post_thumbnail_url( 'yarpp' ); ?>" data-src-retina="<?php the_post_thumbnail_url( 'yarpp-retina' ); ?>">
-                    </a>
-                    <h4 class="jp-relatedposts-post-title">
-                        <a class="jp-relatedposts-post-a" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                    </h4>
-                    <p class="jp-relatedposts-post-date" style="display: block;"><?php the_date(); ?></p>
-                </div>
+					<?php $permalink = get_the_permalink().'?utm_source=related'; ?>
 
-                <?php $i++ ?>
-            <?php endif; ?>
-        <?php endwhile; ?>
+                    <div class="jp-relatedposts-post jp-relatedposts-post<?php echo $i?> jp-relatedposts-post-thumbs">
+                        <a class="jp-relatedposts-post-a" href="<?php echo $permalink; ?>" title="<?php the_title_attribute(); ?>" rel="nofollow" >
+                            <img class="unveil" data-src="<?php the_post_thumbnail_url( 'yarpp' ); ?>" data-src-retina="<?php the_post_thumbnail_url( 'yarpp-retina' ); ?>">
+                        </a>
+                        <h4 class="jp-relatedposts-post-title">
+                            <a class="jp-relatedposts-post-a" href="<?php echo $permalink; ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                        </h4>
+                        <p class="jp-relatedposts-post-date" style="display: block;"><?php the_date(); ?></p>
+                    </div>
+
+					<?php $i++ ?>
+				<?php endif; ?>
+			<?php endwhile; ?>
 
         </div>
     </div>
