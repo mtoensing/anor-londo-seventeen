9c18aa73826a91f0375c5babd3cc557fdb70b479
Marc Tönsing <marc.toensing@gmail.com>
2017-06-30 17:37:37 +0200

    Added template for platfom List

---
+++ page-templates/listsystems.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: Platform List
+ *
+ * Description: A custom game ranking template
+ *
+ */
+get_header();
+?>
+
+<div class="wrap">
+
+	<?php if (have_posts()) : ?>
+		<header class="page-header">
+      <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+      <?php twentyseventeen_edit_link(get_the_ID()); ?>
+		</header><!-- .page-header -->
+	<?php endif; ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+      <div class="entry-content">
+
+          <?php
+
+          $terms = get_terms( 'platform', array(
+	          'hide_empty' => true,
+          ) );
+
+          $html = '<ul class="nobullets">';
+
+          foreach($terms as $term){
+	          $html .= '<li>';
+	          $html .= '<a href="' . get_term_link($term->term_id) . '">';
+
+              if(function_exists('z_taxonomy_image_url')){
+	            $html .= z_taxonomy_image($term->term_id,'thumbnail','',false);
+              }
+	          $html .= '<h2 class="title">'  . $term->name .  '</h2>';
+	          $html .= '</a>';
+	          $html .= '</li>';
+          }
+
+          $html .= '</ul>';
+
+          echo $html;
+          ?>
+
+</div>
+  </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php get_footer();
