136da45be5b0709b96bd6f8f5371ef1524580729
Marc Tönsing <marc.toensing@gmail.com>
2018-03-29 14:10:46 +0200

    Breadcrumbs V2

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -13,9 +13,10 @@
 <div class="site-info">
 	<?php
 	if ( function_exists('yoast_breadcrumb') ) {
-		yoast_breadcrumb('<span id="breadcrumbs">','</span>');
+		yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+		echo '<a href="/impressum/">Impressum</a>';
 	} else {
-		echo '<a href="' . get_home_url(). '">Start</a>';
+		echo '<a href="' . get_home_url(). '">Start</a> | <a href="/impressum/">Impressum</a>';
 	}
-	?> | <a href="/impressum/">Impressum</a>
+	?>
 </div><!-- .site-info -->
