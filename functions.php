2439e76749351ed514a5efa6036494c22137ba38
Marc Tönsing <marc.toensing@gmail.com>
2018-05-10 19:58:44 +0200

    Mich better Breadcrumb integration

--- functions.php
+++ functions.php
@@ -302,3 +302,51 @@ function anorlondo_comments_popup_link( $zero = false, $one = false, $more = fal
 	comments_number( $zero, $one, $more );
 	echo '</a>';
 }
+
+if ( ! function_exists( 'twentyseventeen_entry_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function twentyseventeen_entry_footer() {
+
+		/* translators: used between list items, there is a space after the comma */
+		$separate_meta = __( ', ', 'twentyseventeen' );
+
+		// Get Categories for posts.
+		$categories_list = get_the_category_list( $separate_meta );
+
+		// Get Tags for posts.
+		$tags_list = get_the_tag_list( '', $separate_meta );
+
+		// We don't want to output .entry-footer if it will be empty, so make sure its not.
+		if ( ( ( twentyseventeen_categorized_blog() && $categories_list ) || $tags_list ) || get_edit_post_link() ) {
+
+			echo '<footer class="entry-footer">';
+
+			if ( 'post' === get_post_type() ) {
+				if ( ( $categories_list && twentyseventeen_categorized_blog() ) || $tags_list ) {
+					echo '<span class="cat-tags-links">';
+
+					// Make sure there's more than one category before displaying.
+					if ( $categories_list && twentyseventeen_categorized_blog() ) {
+						echo '<span class="cat-links">' . twentyseventeen_get_svg( array( 'icon' => 'folder-open' ) ) . '<span class="screen-reader-text">' . __( 'Categories', 'twentyseventeen' ) . '</span>' . $categories_list . '</span>';
+					}
+
+					if ( $tags_list && ! is_wp_error( $tags_list ) ) {
+						echo '<span class="tags-links">' . twentyseventeen_get_svg( array( 'icon' => 'hashtag' ) ) . '<span class="screen-reader-text">' . __( 'Tags', 'twentyseventeen' ) . '</span>' . $tags_list . '</span>';
+					}
+
+					if ( function_exists('yoast_breadcrumb') ) {
+						yoast_breadcrumb('<span class="cat-links breadcrumb-links">' . twentyseventeen_get_svg( array( 'icon' => 'bars' ) ) . '<span class="screen-reader-text">' . __( 'Breadcrumbs', 'twentyseventeen' ) . '</span>','</span>');
+					}
+
+					echo '</span>';
+				}
+			}
+
+			twentyseventeen_edit_link();
+
+			echo '</footer> <!-- .entry-footer -->';
+		}
+	}
+endif;
--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -11,12 +11,5 @@
 ?>
 
 <div class="site-info">
-	<?php
-	if ( function_exists('yoast_breadcrumb') ) {
-		yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-		echo '<a href="/impressum/">Impressum</a>';
-	} else {
-		echo '<a href="' . get_home_url(). '">Start</a> | <a href="/impressum/">Impressum</a>';
-	}
-	?>
+    <a href="<?php echo get_home_url(); ?>">Start</a> | <a href="/impressum/">Impressum</a>
 </div><!-- .site-info -->
