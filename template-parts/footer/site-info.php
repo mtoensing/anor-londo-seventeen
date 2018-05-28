d4be766b52db95f9185744a4baf817be73c354ff
Marc Tönsing <marc.toensing@gmail.com>
2018-05-28 19:43:09 +0200

    Changed english privacy policy link

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -15,9 +15,9 @@
     $GDPR_page = '';
 
 	if($GDPR_page_id = get_page_by_title( 'Datenschutzerklärung') ) {
-		$GDPR_page = '| <a href="' . get_page_link($GDPR_page_id) . '">Datenschutz</a>';
-	} elseif ( $GDPR_page_id = get_page_by_title( 'Data Protection Policy') ) {
-		$GDPR_page = ' | <a href="' . get_page_link($GDPR_page_id) . '">Data Protection Policy</a>';
+		$GDPR_page = '| <a href="' . get_page_link($GDPR_page_id) . '">Datenschutzerklärung</a>';
+	} elseif ( $GDPR_page_id = get_page_by_title( 'Privacy Policy') ) {
+		$GDPR_page = ' | <a href="' . get_page_link($GDPR_page_id) . '">Privacy Policy</a>';
 	}
 
 	?>
