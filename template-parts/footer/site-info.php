ec1c7e2924b41260aedc3f1f2684dea0e34bdb69
Marc Tönsing <marc.toensing@gmail.com>
2018-05-28 17:22:10 +0200

    GDPR support in Footer nav

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -11,5 +11,16 @@
 ?>
 
 <div class="site-info">
-    <a href="<?php echo get_home_url(); ?>">Start</a> | <a href="/impressum/">Impressum</a>
+	<?php
+    $GDPR_page = '';
+
+	if($GDPR_page_id = get_page_by_title( 'Datenschutzerklärung') ) {
+		$GDPR_page = '| <a href="' . get_page_link($GDPR_page_id) . '">Datenschutz</a>';
+	} elseif ( $GDPR_page_id = get_page_by_title( 'Data Protection Policy') ) {
+		$GDPR_page = ' | <a href="' . get_page_link($GDPR_page_id) . '">Data Protection Policy</a>';
+	}
+
+	?>
+    <a href="<?php echo get_home_url(); ?>">Start</a> | <a href="/impressum/">Impressum</a> <?php echo $GDPR_page; ?>
+
 </div><!-- .site-info -->
